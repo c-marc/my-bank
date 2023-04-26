74dc01710516cc4ef290fc3344ebbeff9dc6db3c
Marc C <mcar176@gmail.com>
2023-04-26 13:46:13 +0200

    Experiment number formatting

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,5 +1,10 @@
 const formatValue = (value) => {
-  return value.toFixed(2);
+  // return value.toFixed(2);
+  // or even better ?
+  return value.toLocaleString("fr-FR", {
+    minimuFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
 };
 
 export { formatValue };
